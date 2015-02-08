48c30a8a99baebd21b7cafc57f042a40a1042452
Steven Miyakawa SAM <samprofessional@gmail.com>
2015-02-08 11:15:02 -0800

    Make Twitter handle search more robust

--- app/jobs/find_twitter_handle_job.rb
+++ app/jobs/find_twitter_handle_job.rb
@@ -2,12 +2,16 @@ class FindTwitterHandleJob < ActiveJob::Base
   queue_as :default
 
   def perform(options)
-    name = options[:name]
-    query = options[:query]
-    page = Google::Search::Web.new { |search| search.query = "site:twitter.com #{name} #{query}" }.first
-
-    handle = page ? parse(page.uri) : nil
-    Pusher.trigger('twitter_handles', 'result', { name: name, twitter: handle })
+    begin
+      name = options[:name]
+      query = options[:query]
+      page = Google::Search::Web.new { |search| search.query = "site:twitter.com #{name} #{query}" }.first
+      handle = parse(page.uri) if page
+    rescue StandardError => e
+      Rails.logger.error("ERROR RUNNING JOB: #{e}")
+    ensure
+      Pusher.trigger('twitter_handles', 'result', { name: name, twitter: handle })
+    end
   end
 
   TWITTER_HANDLE_REGEX = /twitter.com\/([\w\d]+)/
