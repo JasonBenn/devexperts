ec1d6a44d364f3ca26ba9641ec031bae67291e6c
Steven Miyakawa SAM <samprofessional@gmail.com>
2015-02-08 03:11:34 -0800

    Fix spaces in query bug

--- lib/stackexchange_api.rb
+++ lib/stackexchange_api.rb
@@ -5,11 +5,12 @@ module StackexchangeApi
       BASE_URL = 'https://api.stackexchange.com/2.2'
 
       def top_users(tag, period='month')
+        tag = URI.encode(tag)
         parse(HTTParty.get("#{BASE_URL}/tags/#{tag}/top-answerers/#{period}?site=stackoverflow").parsed_response)
       end
 
       def parse(result)
-        result['items'].map do |user| 
+        result['items'].map do |user|
           clean_user = user.merge(user['user'])
           clean_user.delete('user')
           clean_user
